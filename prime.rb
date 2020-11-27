bf9362962c27171d840a5b2202f904069e6eb052
Michael <wtfdic@hotmail.com>
2020-11-27 03:43:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(number)
   begin = 2
   if number > 1
-    num_range = (begin..number-1).to_a
-    num_range.none? do |test_num|
+    range = (begin..number-1).to_a
+    range.none? do |test_num|
       number % test_num == 0
     end
   else
