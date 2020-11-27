e24336d1d905129f620776b0b1ee0f5b616da4c2
Michael <wtfdic@hotmail.com>
2020-11-27 03:38:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  begin = 2
+  if num > 1
+    num_range = (begin..num-1).to_a
+    num_range.none? do |test_num|
+      num % test_num == 0
+    end
+  else
+    false
+  end
+end
